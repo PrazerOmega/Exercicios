086e6949826d065e2ec22ea4529427bd6eaee47d
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-10-09 18:01:00 -0300

    POINTER_EXERCISE7

--- PointerExercise7.c
+++ PointerExercise7.c
@@ -3,6 +3,13 @@
 /*Receber um valor de array fornecido pelo usuario, dobrar o valor com realloc, solicitar o preenchimento e imprimir depois*/
 
 int main(void){
+    int arraySize = 0;
+
+    printf("Digite o valor de array: ");
+    scanf("%d", &arraySize);
+
+    
+
 
     return 0;
 }
