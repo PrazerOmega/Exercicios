342e59022125e6c0750e8ea6d022e22f8d8bd58a
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-10-10 16:58:01 -0300

    EXERCISE7_POINTERS

--- PointerExercise7.c
+++ PointerExercise7.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*Receber um valor de array fornecido pelo usuario, dobrar o valor com realloc, solicitar o preenchimento e imprimir depois*/
 
 int main(void){
+    int *array = NULL;
     int arraySize = 0;
 
     printf("Digite o valor de array: ");
     scanf("%d", &arraySize);
 
-    
+    array = malloc(arraySize);
 
 
     return 0;
