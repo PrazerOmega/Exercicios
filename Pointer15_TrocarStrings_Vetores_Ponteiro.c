2fa789f2f401656393d418e5124685f24663e935
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-11-05 16:02:15 -0300

    EXERCISE15_TO_EXERCISE16

--- Pointer15_TrocarStrings_Vetores_Ponteiro.c
+++ Pointer15_TrocarStrings_Vetores_Ponteiro.c
@@ -3,11 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-swapPointerVetor(char **str1, char );
+#define TAM 100
+
+void swapPointerVetor(char *str1, char *str2);
 
 int main(void){
+
+char word[2][TAM];
+
+printf("Digite a primeira palavra\n");
+fgets(word[0], TAM, stdin);
+word[0][strcspn(word[0], "\n")] = '\0';
+
+printf("Digite a segunda palavra\n");
+fgets(word[1], TAM, stdin);
+word[1][strcspn(word[1], "\n")] = '\0';
+
+swapPointerVetor(word[0],word[1]);
+
+printf("1 palavra: %s\n2 palavra: %s\n",word[0], word[1]);
 
 
 
     return 0;
+}
+
+
+void swapPointerVetor(char *str1, char *str2){
+char temp[TAM];
+strcpy(temp,str1);
+strcpy(str1, str2);
+strcpy(str2, temp); 
 }
---
+++ Pointer16_Troca_posicao_Vetor.c
@@ -0,0 +1,23 @@
+/*Vetor de Strings Dinâmico com Troca*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 100
+
+void trocar(char **v, int i, int j);
+
+int main(void){
+
+    char *words = NULL;
+
+
+    printf("Diga quantas palavras voce deseja escrever:\n");
+
+
+
+
+    return 0;
+}
+
+void trocar(char **v, int i, int j);
