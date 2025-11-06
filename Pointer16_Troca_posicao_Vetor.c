0ab6383282568652a8cc8380e8d3e74eeaa8e0e5
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-11-06 17:01:53 -0300

    Update_Pointer16

--- Pointer16_Troca_posicao_Vetor.c
+++ Pointer16_Troca_posicao_Vetor.c
@@ -9,10 +9,43 @@ void trocar(char **v, int i, int j);
 
 int main(void){
 
-    char *words = NULL;
+    char **cVetor = NULL;
+    int vetTam = 0;
 
 
     printf("Diga quantas palavras voce deseja escrever:\n");
+    if(scanf("%d", &vetTam) != 1 || vetTam <= 0){
+        fprintf(stderr,"Valores incorretos");
+    }
+
+
+    cVetor = malloc(vetTam * sizeof(char *));
+    if(cVetor == NULL){
+        perror("Erro ao alocar memoria!");
+        return 1;
+    }
+
+    for (size_t i = 0; i < vetTam; i++)
+    {
+        cVetor[i] = malloc(TAM);
+        if (cVetor[i] == NULL)
+        {
+            perror("Erro ao alocar memoria!");
+            return 0;
+        }
+        
+    }
+    
+
+    printf("Tamanho de vetores %d", size);
+
+   // printf("Preencha o vetor com as palavras desejadas, até o maximo de 100 caracteres:\n");
+
+   /* for(size_t i = 0; i < vetTam; i++){
+
+        fgets(cVetor[i])
+
+    }*/
 
 
 
