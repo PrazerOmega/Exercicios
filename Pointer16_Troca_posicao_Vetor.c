355bdcf48d297947af94940dac7a780a6655cb90
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-11-07 16:54:57 -0300

    FINISH_EXER16-START_EXER17

--- Pointer16_Troca_posicao_Vetor.c
+++ Pointer16_Troca_posicao_Vetor.c
@@ -37,20 +37,30 @@ int main(void){
     }
     
 
-    printf("Tamanho de vetores %d", size);
+    printf("Preencha o vetor com as palavras desejadas, ate o maximo de 100 caracteres:\n\nPalavra 1:");
+    fgets(cVetor[0], TAM, stdin);
+    cVetor[0][strcspn(cVetor[0], "\n")] = '\0';
 
-   // printf("Preencha o vetor com as palavras desejadas, até o maximo de 100 caracteres:\n");
+    for(size_t i = 1; i < vetTam; i++){
 
-   /* for(size_t i = 0; i < vetTam; i++){
+    printf("Palavra %zu: ", i+1);
+     fgets(cVetor[i], TAM, stdin);
+     cVetor[i][strcspn(cVetor[i], "\n")] = '\0';
+    }
 
-        fgets(cVetor[i])
+    trocar(cVetor,1,3);
 
-    }*/
-
-
+for (size_t i = 0; i < vetTam; i++)
+{
+    printf("Palavra %zu: %s\n", i+1, cVetor[i]);
+}
 
 
     return 0;
 }
 
-void trocar(char **v, int i, int j);
+void trocar(char **v, int i, int j){
+    char *temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
---
+++ Pointer17_OrdenarAlfabetico_Vetor.c
@@ -0,0 +1,62 @@
+/*Criar um vetor de X palavras(usuario decide), alocar strings em um vetor e ordenalas em ordem alfabetica*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 100
+
+void alfabetico(char **vetor);
+
+int main(void){
+
+    char **vetWord = NULL;
+    int vetSize = 0;
+
+    printf("Quantia de palavras a serem escritas:\n");
+    scanf("%d", &vetSize);
+
+    vetWord = malloc(vetSize * sizeof(char*));
+    if(vetWord == NULL){
+        perror("Erro ao alocar memoria!");
+        return 1;
+    }
+
+    printf("Escreva as palavras");
+
+    for (size_t i = 0; i < vetSize; i++)
+    {
+        printf("Palavra %zu\n", i+1);
+        fgets(vetWord[i], TAM, stdin);
+        vetWord[i][strcspn(vetWord[i],"\n")] = '\0';
+    }
+    
+
+
+
+    return 0;
+}
+
+    void alfabetico(char **vetor, int qntWords){
+
+    char *order = malloc(qntWords * sizeof(char));
+    if (order == NULL)
+    {
+        perror("Erro ao alocar memoria!");
+        return 1;
+    }
+    
+    for (size_t i = 0; i < qntWords; i++)
+    {
+       for (size_t j = 0; j < qntWords; j++)
+       {
+        int result = strcmp(*vetor[i], *vetor[j]);
+       }
+       
+    }
+    
+    
+
+
+
+    }
